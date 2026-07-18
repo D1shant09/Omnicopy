772ff6c4596e84f67b008881821a654ef8b76527
D1shant09 <dishantfaujdar@gmail.com>
2026-07-18 15:21:17 +0530

    adding database

--- main.py
+++ main.py
@@ -1,73 +1,116 @@
-from fastapi import FastAPI, Request, HTTPException
-from datetime import datetime
-from typing import Any
+from contextlib import asynccontextmanager
+from typing import Any, Annotated
 from random import randint
-from fastapi import Response
-app = FastAPI(root_path="/api/v1")
+from datetime import datetime, timezone
+
+from fastapi import FastAPI, HTTPException, Depends, Response
+from sqlmodel import SQLModel, Session, create_engine, select, Field
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None=Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=True, index=True)
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Campaign)).first():
+            session.add_all([
+                Campaign(name="Summer Launch", due_date=datetime.now()),
+                Campaign(name="Black Fridat", due_date=datetime.now())
+            ])
+            session.commit()
+    yield
+
+
+app = FastAPI(root_path="/api/v1", lifespan=lifespan)
+
 
 @app.get("/")
 async def root():
-    return {"message": "hello world"}
+    return {"message": "Hello world"}
 
-data : Any = [
+
+data: Any = [
     {
         "campaign_id": 1,
         "name": "Summer Launch",
         "due_date": datetime.now(),
-        "created_at": datetime.now()
+        "created_at": datetime.now(),
     },
     {
         "campaign_id": 2,
         "name": "Black Friday",
         "due_date": datetime.now(),
-        "created_at": datetime.now()
-    }
+        "created_at": datetime.now(),
+    },
 ]
 
-@app.get("/campaigns")
-async def read_campaigns():
-    return {"campaigns": data}
 
-@app.get("/campaigns/{id}")
-async def read_campaign(id: int):
-    for campaign in data:
-        if campaign.get("campaign_id") == id:
-            return {"campaign": campaigns}
-    return HTTPException(status_code=404)
-
-@app.post("/campaigns")
-async def create_campaigns(body: dict[str, Any]):
-
-    new : Any = { 
-        "campaign_id": randint(100, 1000),
-        "name": body.get("name"), 
-        "due_date": body.get("due_date"), 
-        "created_at": datetime.now()
-        }
-    data.append(new)
-    return {"campaign": new}
-
-@app.put("/campaigns/{id}",)
-async def update_campaign(id: int, body: dict[str, Any]):
-
-    for index, campaign in enumerate(data):
-        if campaign.get("campaign_id") == id:     
-            updated : Any = { 
-                "campaign_id": id,
-                "name": body.get("name"), 
-                "due_date": body.get("due_date"),
-                "created_at": campaign.get("created_at")
-            }
-            data[index] = updated
-            return {"campaign": updated}
-    raise HTTPException(status_code=404)
-
-
-@app.delete("/campaigns/{id}",)
-async def update_campaign(id: int):
-
-    for index, campaign in enumerate(data):
-        if campaign.get("campaign_id") == id:     
-            data.pop(index)
-            return Response(status_code=204)
-    raise HTTPException(status_code=404)
+# @app.get("/campaigns")
+# async def read_campaigns():
+#     return {"campaigns": data}
+#
+#
+# @app.get("/campaigns/{id}")
+# async def read_campaign(id: int):
+#     for campaign in data:
+#         if campaign.get("campaign_id") == id:
+#             return {"campaign": campaign}
+#     raise HTTPException(status_code=404)
+#
+#
+# @app.post("/campaigns")
+# async def create_campaigns(body: dict[str, Any]):
+#     new: Any = {
+#         "campaign_id": randint(100, 1000),
+#         "name": body.get("name"),
+#         "due_date": body.get("due_date"),
+#         "created_at": datetime.now(),
+#     }
+#     data.append(new)
+#     return {"campaign": new}
+#
+#
+# @app.put("/campaigns/{id}")
+# async def update_campaign(id: int, body: dict[str, Any]):
+#     for index, campaign in enumerate(data):
+#         if campaign.get("campaign_id") == id:
+#             updated: Any = {
+#                 "campaign_id": id,
+#                 "name": body.get("name"),
+#                 "due_date": body.get("due_date"),
+#                 "created_at": campaign.get("created_at"),
+#             }
+#             data[index] = updated
+#             return {"campaign": updated}
+#     raise HTTPException(status_code=404)
+#
+#
+# @app.delete("/campaigns/{id}")
+# async def delete_campaign(id: int):
+#     for index, campaign in enumerate(data):
+#         if campaign.get("campaign_id") == id:
+#             data.pop(index)
+#             return Response(status_code=204)
+#     raise HTTPException(status_code=404)
