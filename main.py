951151c0e56b8aaffbc7d6094e5d1a65a1ce8716
D1shant09 <dishantfaujdar@gmail.com>
2026-07-21 22:57:27 +0530

    Pagination

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
-from typing import Annotated, Generic, TypeVar
+from typing import Annotated, Generic, TypeVar, Optional
 from datetime import datetime, timezone
 from pydantic import BaseModel
+from sqlalchemy import func
 
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Query, Request
 from sqlmodel import SQLModel, Session, create_engine, select, Field
 
 class Campaign(SQLModel, table=True):
@@ -53,10 +54,36 @@ T = TypeVar("T")
 class Response(BaseModel, Generic[T]):
     data: T
 
-@app.get("/campaigns", response_model=Response[list[Campaign]])
-async def read_campaigns(session: SessionDep):
-    data = session.exec(select(Campaign)).all()
-    return {"data": data}
+class PaginatedResponse(BaseModel, Generic[T]):
+    data: T
+    next: Optional[str]
+    prev: Optional[str]
+    count: int
+
+@app.get("/campaigns", response_model=PaginatedResponse[list[Campaign]])
+async def read_campaigns(request: Request, session: SessionDep, page: int = Query(1, ge=1), page_size: int = Query(20, ge=1)):
+    limit = page_size
+    offset = (page-1) * limit
+    data = session.exec(select(Campaign).order_by(Campaign.campaign_id).offset(offset).limit(limit)).all()
+    base_url = str(request.url).split('?')[0]
+
+    total = session.exec(select(func.count()).select_from(Campaign)).one()
+
+    if offset + limit < total:
+        next_url = f"{base_url}?page={page+1}&page_size={limit}"
+    else:
+        next_url = None 
+    if page > 1:
+        prev_url = f"{base_url}?page={page-1}&page_size={limit}"
+    else:
+        prev_url = None
+
+    return {
+        "count": total,
+        "next": next_url,
+        "prev": prev_url,
+        "data": data
+    }
 
 @app.get("/campaigns/{id}", response_model=Response[Campaign])
 async def read_campaigns(id: int, session: SessionDep):
